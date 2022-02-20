5ccd3592b8eaccd7c86106f6b92b3f4c030ad318
Grohmur <zverma92@gmail.com>
2022-02-20 12:52:12 +0300

    Initial Commit

---
+++ SwiftUIColorSlider/SwiftUIColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorSliderApp.swift
+//  SwiftUIColorSlider
+//
+//  Created by Михаил Зверьков on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
